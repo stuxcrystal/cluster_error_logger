2f165e06f8823a4958fe5da5f2ee1b2ebe18f152
Ninigi <fabian.zitter@gmail.com>
2014-09-10 15:32:36 +0200

    added error_logger

---
+++ lib/error_logger.rb
@@ -0,0 +1,9 @@
+class ErrorLogger < Logger
+	def format_message(severity, timestamp, progname, msg)
+		"#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
+	end
+end
+ 
+logfile = File.open("#{Rails.root}/log/error.log", 'a') # create log file
+logfile.sync = true # automatically flushes data to file
+Log = ErrorLogger.new(logfile) # constant accessible anywhere
