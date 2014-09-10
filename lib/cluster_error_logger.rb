11886511a20e19053f6cc0fbc57bc50cfdab2b2b
Ninigi <fabian.zitter@gmail.com>
2014-09-10 15:43:07 +0200

    still playing

--- lib/cluster_error_logger.rb
+++ lib/cluster_error_logger.rb
@@ -8,28 +8,4 @@ module ClusterErrorLogger
 	  Log.error infos.join("\n")
 	  raise exception
 	end
-
-	class CatchJsonParseErrors
-	  def initialize(app)
-	    @app = app
-	  end
-
-	  def call(env)
-	    begin
-	      @app.call(env)
-	    rescue JSON::ParserError => error
-	      infos = ["\tTriggered in Ticket Machine app: #{env['REQUEST_METHOD']} #{env['REQUEST_PATH']}"]
-	      infos << "\tPARAMS:"
-	      env['rack.session'].each do |key, val|
-	        infos << "\t\t#{key}: #{val}"
-	      end
-	      infos << "\tCookies"
-	      env[ "action_dispatch.request.unsigned_session_cookie"].each do |key, val|
-	        infos << "\t\t#{key}: #{val}"
-	      end
-	      Log.error infos.join("\n")
-	      raise error
-	    end
-	  end
-	end
 end
