34950f8e7749ccb738be639f9e44697e2e8a1aed
Ninigi <fabian.zitter@gmail.com>
2014-09-10 15:57:01 +0200

    changed require

--- lib/error_logger.rb
+++ lib/error_logger.rb
@@ -4,6 +4,6 @@ class ErrorLogger < Logger
 	end
 end
  
-logfile = File.open("error.log", 'a') # create log file
+logfile = File.open("#{Rails.root}/../cluster_log/error.log", 'a') # create log file
 logfile.sync = true # automatically flushes data to file
 Log = ErrorLogger.new(logfile) # constant accessible anywhere
