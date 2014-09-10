c31d574872c4971f10fba436944bf414b15588c3
Ninigi <fabian.zitter@gmail.com>
2014-09-10 17:00:12 +0200

    changed path to logfile

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -5,7 +5,7 @@ module ClusterErrorLogger
     initializer "cluster_error_logger.configure_rails_initialization" do
 		  config.app_middleware.insert_before ActionDispatch::ParamsParser, "CatchJsonParseErrors"
 
-		  logfile = File.open(File.expand_path("#{Rails.root}/../cluster_log/error.log", 'a')) # create log file
+		  logfile = File.open(File.expand_path("#{Rails.root}/../../../{cluster_log/error.log", 'a')) # create log file
 			logfile.sync = true # automatically flushes data to file
 			Log = ErrorLogger.new(logfile) # constant accessible anywhere
 		end
