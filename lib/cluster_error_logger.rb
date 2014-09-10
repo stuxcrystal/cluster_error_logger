3c4a8c65a4dae1f9fefb7259971ba93fd74f152f
Ninigi <fabian.zitter@gmail.com>
2014-09-10 16:16:17 +0200

    pplayin

--- lib/cluster_error_logger.rb
+++ lib/cluster_error_logger.rb
@@ -2,6 +2,7 @@ require_relative "error_logger"
 
 module ClusterErrorLogger
 	def log_error(exception)
+		puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 	  infos = []
 	  infos << "Error raised when executing #{request.method} #{request.fullpath}"
 	  infos << "Exception trace: \n\t#{exception.backtrace.take(5).join("\n\t")}"
