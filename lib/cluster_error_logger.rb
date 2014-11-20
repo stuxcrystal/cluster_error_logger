227f65c6d5cd652a75e1249a71c1d3eb16118bd8
Ninigi <fabian.zitter@gmail.com>
2014-11-20 09:33:59 +0100

    new log method 'log_exception', log_error changed to non-exceptional error log

--- lib/cluster_error_logger.rb
+++ lib/cluster_error_logger.rb
@@ -3,7 +3,7 @@ require_relative "catch_json_parse_errors"
 require_relative "railtie"
 
 module ClusterErrorLogger
-	def log_error(exception)
+	def log_exception(exception)
 	  infos = []
 	  infos << "Error raised when executing #{request.method} #{request.fullpath}"
 	  infos << "Exception trace: \n\t#{exception.backtrace.take(5).join("\n\t")}"
@@ -11,4 +11,15 @@ module ClusterErrorLogger
 	  $log.error infos.join("\n")
 	  raise exception
 	end
+
+  def log_error(*error_messages)
+    infos = []
+    infos << "Non-Exception Error raised when executing #{request.method} #{request.fullpath}"
+    infos << "Error trace:"
+    error_messages.each do |e_message|
+      infos << "\n\t#{e_message}"
+    end
+    infos << "\n\n"
+    $log.error infos.join("\n")
+  end
 end
