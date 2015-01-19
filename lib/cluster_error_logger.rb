aa2fac5b0ea431f72ee7261f9efba5905f5779a7
Ninigi <fabian.zitter@gmail.com>
2015-01-19 17:57:44 +0100

    create log dir if not existing

--- lib/cluster_error_logger.rb
+++ lib/cluster_error_logger.rb
@@ -4,6 +4,7 @@ require_relative "catch_json_parse_errors"
 require_relative "railtie"
 require 'gem_config'
 require "core_log_methods"
+require 'fileutils'
 
 ##
 # Use these Methods to log your errors to a central File. Best practice would be to include into application_controller and rescue exceptions through +:log_exception+
@@ -11,6 +12,7 @@ module ClusterErrorLogger
   include GemConfig::Base
 
   with_configuration do
+    FileUtils.mkdir_p "#{Rails.root}/../../cluster_log"
     has :log_dir, :classes => String, :default => File.expand_path("#{Rails.root}/../../cluster_log")
   end
 end
