fc54e95826b07773010812e1fa140cf22b700167
Roland Netzsch <stuxcrystal@users.noreply.github.com>
2017-08-24 17:39:47 +0200

    Update railtie.rb

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,18 +1,16 @@
 require "rails"
 
 module ClusterErrorLogger
-	class Railtie < Rails::Railtie
+  class Railtie < Rails::Railtie
     initializer "cluster_error_logger.configure_rails_initialization" do
-		  config.app_middleware.insert_before ActionDispatch::ParamsParser, "CatchJsonParseErrors"
-
-		  logfile = File.open("#{ClusterErrorLogger.configuration.log_dir}/error.log", 'a') # create log file
+      config.app_middleware.insert_before ActionDispatch::ParamsParser, "CatchJsonParseErrors"
+      Dir.mkdir("#{ClusterErrorLogger.configuration.log_dir}") if !Dir.exists? "#{ClusterErrorLogger.configuration.log_dir}"
+      logfile = File.open("#{ClusterErrorLogger.configuration.log_dir}/error.log", 'a') # create log file
       info_logfile = File.open("#{ClusterErrorLogger.configuration.log_dir}/info.log", 'a')
-
-			logfile.sync = true # automatically flushes data to file
+      logfile.sync = true # automatically flushes data to file
       info_logfile.sync = true
-      
-			$log = ErrorLogger.new(logfile) # constant accessible anywhere
+      $log = ErrorLogger.new(logfile) # constant accessible anywhere
       $arb_log = ArbitraryStuffLogger.new(info_logfile)
-		end
-	end
+     end
+   end
 end
